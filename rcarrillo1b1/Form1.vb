8afab97e02961fca66589abc79fcdec1ee0cc444
Ramon Carrillo <ww3519og@my.southeastmn.edu>
2016-08-30 20:08:06 -0500

    Intial commit Ex1B1

--- rcarrillo1b1/Form1.vb
+++ rcarrillo1b1/Form1.vb
@@ -1,5 +1,10 @@
 ﻿Public Class frmSplash
     Private Sub tmrExit_Tick(sender As Object, e As EventArgs) Handles tmrExit.Tick
         Me.Close()
+        ' redone and open it works
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
     End Sub
 End Class
