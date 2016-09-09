3493f1b9dc2259dffd525089992f9512d22d8a7c
Ramon Carrillo <ww3519og@my.southeastmn.edu>
2016-09-08 23:44:27 -0500

    added ex1d

---
+++ rcarrillo1d1/Main Form.vb
@@ -0,0 +1,36 @@
+﻿'Name: Bakery Project
+' Purpose: Calculates the total number of items sold and the total sales 
+' Programmer: Ramon Carrillo on 9/8/16
+
+
+Public Class frmMain
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub lblTotalSales_Click(sender As Object, e As EventArgs) Handles lblTotalSales.Click
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtDonuts.Text = String.Empty
+        txtMuffins.Text = String.Empty
+        lblTotalItems.Text = String.Empty
+        lblTotalSales.Text = String.Empty
+        ' send the focus to the Doughnuts box
+        txtDonuts.Focus()
+
+    End Sub
+
+    Private Sub btnPrint_Click(sender As Object, e As EventArgs) Handles btnPrint.Click
+
+
+    End Sub
+
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+        ' calculate number of items sold and totals sales
+        lblTotalItems.Text = Val(txtDonuts.Text) + Val(txtMuffins.Text)
+        lblTotalSales.Text = Val(lblTotalItems.Text) * 0.05
+        lblTotalSales.Text = Format(lblTotalSales.Text, "currency")
+    End Sub
+End Class
