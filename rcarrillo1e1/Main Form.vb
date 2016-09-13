d8f7cc9572ee9fd664c21949d5a4b7936d2282c3
Ramon Carrillo <ww3519og@my.southeastmn.edu>
2016-09-12 23:50:14 -0500

    Added ex1e

---
+++ rcarrillo1e1/Main Form.vb
@@ -0,0 +1,28 @@
+﻿' Name: Richard County Property Tax Project
+' Purpose: Calculates Property Tax
+' Author: Ramon Carrillo on 9/12/16
+
+
+
+
+
+Public Class frmMain
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtAssessed.Text = String.Empty
+        lblTax.Text = String.Empty
+        txtAssessed.Focus()
+    End Sub
+
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+        lblTax.Text = Val(txtAssessed.Text) * 0.015
+        lblTax.Text = Format(lblTax.Text, "currency")
+    End Sub
+End Class
